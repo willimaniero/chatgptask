8c827f007db20129270ea3ee7cf9b8cb9a068de0
Willi Maniero <118349466+willimaniero@users.noreply.github.com>
2023-05-21 16:17:28 -0300

    Initial Commit

---
+++ ChatGPTask/ChatGPTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTaskApp.swift
+//  ChatGPTask
+//
+//  Created by Willi Maniero on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTask/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatGPTask
+//
+//  Created by Willi Maniero on 21/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
